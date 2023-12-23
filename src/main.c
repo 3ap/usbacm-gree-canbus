eef9f8b48fa054c728800720e6f8888acd93067d
Sergey Nazaryev <sergey@nazaryev.com>
2023-12-23 17:41:58 +0200

    Rename my-project -> src, remove my-common-code

--- src/main.c
+++ src/main.c
@@ -2,14 +2,7 @@
 #include "task.h"
 #include "queue.h"
 
-#include "api.h"
-#include "api-asm.h"
-
 static void main_task(void *args __attribute__((unused))) {
-       /* add your own code */
-       uint32_t rev = 0xaabbccdd;
-       rev = rev_bytes(rev);
-       my_func(rev);
 }
 
 int main(void) {
