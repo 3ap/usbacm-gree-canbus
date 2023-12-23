ff8ac9168049c58b5b210f7491903257ff51534e
Sergey Nazaryev <sergey@nazaryev.com>
2023-12-23 17:11:55 +0200

    Integrate FreeRTOS into example

--- my-project/my-project.c
+++ my-project/my-project.c
@@ -1,9 +1,19 @@
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
 #include "api.h"
 #include "api-asm.h"
 
+static void main_task(void *args __attribute__((unused))) {
+       /* add your own code */
+       uint32_t rev = 0xaabbccdd;
+       rev = rev_bytes(rev);
+       my_func(rev);
+}
+
 int main(void) {
-	/* add your own code */
-	uint32_t rev = 0xaabbccdd;
-	rev = rev_bytes(rev);
-	return my_func(rev);
+       xTaskCreate(main_task, "MAIN", 100, NULL, configMAX_PRIORITIES-1, NULL);
+       vTaskStartScheduler();
+       for (;;);
 }
